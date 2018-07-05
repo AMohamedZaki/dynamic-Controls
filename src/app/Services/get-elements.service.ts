5c6d0bd4b1ff6443a3f3c9fe703e05168ec6c080
Abdelmoneim Mohamed <a.mohamed@nt-me.com>
2018-07-05 13:58:20 +0200

    start new implementation

--- src/app/Services/get-elements.service.ts
+++ src/app/Services/get-elements.service.ts
@@ -4,6 +4,7 @@ import { DropDown } from '../model/dropDown';
 import { ElementConvertService } from './element-convert.service';
 import { DatePickerElement } from '../model/datepicker';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Panel } from '../model/panel';
 
 @Injectable()
 export class GetElementsService {
@@ -19,54 +20,52 @@ export class GetElementsService {
 
 
   getElements() {
-    const elemnents: any[] = [
-
-      new TextBoxElement({
-        Key: 'firstName',
-        Label: 'First name',
-        value: 'National Technology',
-        required: true,
-        visible: true,
-        id: 0,
-        events:
-          [
-            { Name: 'click', callBack: () => { this.elementConvertService.test(); } },
-            { Name: 'change', callBack: el => { console.log(el.target.value); } }
+    const elemnents: Panel[] = [{
+      titel: 'Patient',
+      elementList: [
+        new TextBoxElement({
+          Key: 'firstName',
+          Label: 'First name',
+          value: 'National Technology',
+          required: true,
+          visible: true,
+          id: 0,
+          events:
+            [
+              { Name: 'click', callBack: () => { this.elementConvertService.test(); } },
+              { Name: 'change', callBack: el => { console.log(el.target.value); } }
+            ]
+        }),
+        new DatePickerElement({
+          Key: 'dateTimePicker',
+          Label: 'Select Date',
+          required: true,
+          visible: false,
+          id: 3
+        }),
+        new DropDown({
+          Key: 'City',
+          Label: 'Choose your City ?',
+          required: true,
+          visible: false,
+          id: 4,
+          options: [
+            { key: 'cairo', value: 'Cairo' },
+            { key: 'nasr', value: 'Nasr' },
+            { key: 'helwan', value: 'Helwan' },
+            { key: 'zagazeg', value: 'Zagazeg' }
           ]
-      }),
-
-
-      new DatePickerElement({
-        Key: 'dateTimePicker',
-        Label: 'Select Date',
-        required: true,
-        visible: false,
-        id: 3
-      }),
-
-      new DropDown({
-        Key: 'City',
-        Label: 'Choose your City ?',
-        required: true,
-        visible: false,
-        id: 4,
-        options: [
-          { key: 'cairo', value: 'Cairo' },
-          { key: 'nasr', value: 'Nasr' },
-          { key: 'helwan', value: 'Helwan' },
-          { key: 'zagazeg', value: 'Zagazeg' }
-        ]
-      }),
-      new TextBoxElement({
-        Key: 'lastName',
-        Label: 'Last name',
-        value: 'Zaki',
-        required: true,
-        visible: false,
-        id: 3
-      })
-
-    ];
+        }),
+        new TextBoxElement({
+          Key: 'lastName',
+          Label: 'Last name',
+          value: 'Zaki',
+          required: true,
+          visible: false,
+          id: 3
+        })
+      ]
+    }];
 
     return elemnents;
 
